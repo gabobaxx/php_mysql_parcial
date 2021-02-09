93eac5a5f476443ed8ca2c15518e7a48abd756ac
Gabriel Bencomo <59029788+gabrielba15@users.noreply.github.com>
2021-02-09 11:05:53 -0400

    version 1 done, all is up to date

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 
 <!-- HTML -->
 <main class="container">
-  <!-- ENTRADA: Formularios y tabla -->
+  <!-- ENTRADA: Formulario y tabla -->
     <div class="row pt-4">
       <!-- PRIMERA COLUMNA -->
         <div class="col-md-7">
@@ -37,14 +37,16 @@
                     <label for="brand" class="form-label">
                       <strong>*</strong>Marca del Producto
                     </label>
-                    <input type="text" name="product_brand"  id="brand" class="form-control"  placeholder="Maximo 50 caracteres" maxlength="50" required>
+                    <input type="text" name="product_brand"  id="brand" class="form-control"  
+                    placeholder="Maximo 50 caracteres" maxlength="50" required>
                   </div>
                 <!-- TERCER INPUT -->
                   <div class="mb-3">
                     <label for="size" class="form-label">
                       Contenido Neto
                     </label>
-                    <input type="number" name="product_size"  id="size" class="form-control" placeholder="Solo enteros positivos" min="1">
+                    <input type="number" name="product_size"  id="size" class="form-control" 
+                    placeholder="Solo enteros positivos" min="1">
                   </div>
                 <!-- CUARTO INPUT -->
                   <div>
@@ -100,21 +102,11 @@
         </div>
       <!-- SEGUNDA COLUMNA -->
         <div class="col-md-5">
-          <!-- OPCIONES -->
-            <select name="filtrado">
-              <option value="product_name">Nombre del Producto</option>
-              <option value="product_brand">Marca del Producto</option>
-              <option value="date">Fecha de Ingreso</option>
-            </select>
-          <!-- BOTON -->
-            <div class="form-group">
-              <input type="submit" name="filtrar" class="btn btn-success btn-block" value="Buscar">
-            </div>
-          <!-- TABLA -->
             <table class="table table-bordered">
               <!-- CABECERA DE LA TABLA -->
                 <thead>
                   <tr class="text-center">
+                    <th>ID</th>
                     <th>Producto</th>
                     <th>Marca</th>
                     <th>Contenido Neto (unidad)</th>
@@ -122,12 +114,13 @@
                     <th>Precio de Compra (unidad)</th>
                     <th>Porcentaje de Ganancia</th>
                     <th>Precio Total (unidad)</th>
+                    <th>Acción</th>
                   </tr>
                 </thead>
               <!-- CUERPO DE LA TABLA -->
                 <tbody> 
                   <?php 
-                    $query = "SELECT `product_name` AS PRODUCT, `product_brand` AS BRAND, product_size AS CONTENIDO_NETO, measure_unit AS PRESENTACION, units AS UNITS, buy_price AS BUY_PRICE, earn AS EARN, created_at AS DATE FROM inventario ORDER BY product_name";
+                    $query = "SELECT id, `product_name` AS PRODUCT, `product_brand` AS BRAND, product_size AS CONTENIDO_NETO, measure_unit AS PRESENTACION, units AS UNITS, buy_price AS BUY_PRICE, earn AS EARN, created_at AS DATE FROM inventario";
                     $resultados = mysqli_query($conn, $query);
                     $price = 0;
                     $units = 0;
@@ -141,6 +134,7 @@
                       $IVA = $total_price * 0.16;
                       $total_price = $total_price + $IVA; ?>
                       <tr>
+                        <td> <?php echo $row['id']; ?> </td>
                         <td> <?php echo $row['PRODUCT']; ?> </td>
                         <td> <?php echo $row['BRAND']; ?> </td>
                         <td> <?php echo $row['CONTENIDO_NETO'], $row['PRESENTACION']; ?> </td>
@@ -149,13 +143,25 @@
                         <?php }else{ ?>
                             <td> <?php echo $row['UNITS'];?> unidad</td> 
                         <?php }; ?>
-                        <td> <?php echo $row['BUY_PRICE'];?>$</td>
-                        <td> <?php echo $row['EARN'];?>%</td>
-                        <td> <?php echo $total_price ?>$</td>
+                        <td> <?php echo $row['BUY_PRICE'];?> $</td>
+                        <td> <?php echo $row['EARN'];?> %</td>
+                        <td> <?php echo $total_price  / (int)$row['UNITS']?> $</td>
+
+                        <td>
+                          <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-primary">
+                            <i class="bi bi-pencil-fill"></i>
+                            ACTUALIZAR
+                          </a> 
+                          <br><br>
+                          <a href="delete.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                            <i class="bi bi-trash-fill"></i>
+                            ELIMINAR
+                          </a>
+                        </td>
                       </tr>
                   <?php }; ?>        
                 </tbody>
-            </table>
+            </table>  
           <!-- ICONO 1 -->
             <h3 class="mt-5 text-danger text-center">
               <!-- <i class="bi bi-arrow-90deg-left"></i> -->
@@ -169,6 +175,245 @@
               <i class="bi bi-arrow-left-circle-fill"></i>
             </h3>    
         </div>
+    </div>
+  <!-- SEGUNDA: Formularios y tabla -->
+    <div class="row mt-10">
+      <!-- FILTRO -->
+        <form action="index.php/#table" method="POST">
+          <!-- OPCIONES -->
+            <select name="filtrado">
+              <option value="product_name">Nombre del Producto</option>
+              <option value="product_brand">Marca del Producto</option>
+              <option value="date">Fecha de Ingreso</option>
+            </select>
+          <!-- BOTON -->
+            <div class="form-group">
+              <input type="submit" name="filter" class="btn btn-success btn-block" value="Buscar">
+            </div>
+        </form>
+        <?php if(isset($_POST['filter'])){ ?>
+        <?php $filter = $_POST['filtrado']; ?>
+        <?php if($filter == 'product_name'){ ?>
+      <!-- TABLA -->
+          <table class="table table-bordered" id="table">
+            <caption>Nombre del Producto</caption>
+            <!-- CABECERA DE LA TABLA -->
+              <thead>
+                <tr class="text-center">
+                  <th>Producto</th>
+                  <th>Marca</th>
+                  <th>Contenido Neto (unidad)</th>
+                  <th>Cantidad Entrante (unidad)</th>
+                  <th>Precio de Compra (unidad)</th>
+                  <th>Porcentaje de Ganancia</th>
+                  <th>Precio Total (unidad)</th>
+                  <th>Acción</th>
+                </tr>
+              </thead>
+            <!-- CUERPO DE LA TABLA -->
+              <tbody> 
+                <?php 
+                  $query = "SELECT id, `product_name` AS PRODUCT, `product_brand` AS BRAND, product_size AS CONTENIDO_NETO, measure_unit AS PRESENTACION, units AS UNITS, buy_price AS BUY_PRICE, earn AS EARN, created_at AS DATE FROM inventario ORDER BY product_name";
+                  $resultados = mysqli_query($conn, $query);
+                  $price = 0;
+                  $units = 0;
+                  $earn = 0;
+                  $IVA = 0;
+                  $total_price = 0;
+                  while($row = mysqli_fetch_assoc($resultados)) { 
+                    $price = (float)$row['BUY_PRICE']* (int)$row['UNITS'];
+                    $earn = $row['EARN']; 
+                    $total_price = $price + ($price * $earn) / 100 ;
+                    $IVA = $total_price * 0.16;
+                    $total_price = $total_price + $IVA; ?>
+                    <tr>
+                      <td> <?php echo $row['PRODUCT']; ?> </td>
+                      <td> <?php echo $row['BRAND']; ?> </td>
+                      <td> <?php echo $row['CONTENIDO_NETO'], $row['PRESENTACION']; ?> </td>
+                      <?php if( (int)$row['UNITS'] > 1){ ?>
+                          <td> <?php echo $row['UNITS'];?> unidades</td>
+                      <?php }else{ ?>
+                          <td> <?php echo $row['UNITS'];?> unidad</td> 
+                      <?php }; ?>
+                      <td> <?php echo $row['BUY_PRICE'];?>$</td>
+                      <td> <?php echo $row['EARN'];?>%</td>
+                      <td> <?php echo $total_price  / (int)$row['UNITS']?>$</td>
+
+                      <td>
+                        <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-primary">
+                          <i class="bi bi-pencil-fill"></i>
+                          ACTUALIZAR
+                        </a> 
+                        <br><br>
+                        <a href="delete.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                          <i class="bi bi-trash-fill"></i>
+                          ELIMINAR
+                        </a>
+                      </td>
+                    </tr>
+                <?php }; ?>        
+              </tbody>
+          </table>
+          <form action="index.php/#table" method="POST">
+            <!-- OPCIONES -->
+              <select name="filtrado">
+                <option value="product_name" selected>Nombre del Producto</option>
+                <option value="product_brand">Marca del Producto</option>
+                <option value="date">Fecha de Ingreso</option>
+              </select>
+            <!-- BOTON -->
+              <div class="form-group">
+                <input type="submit" name="filter" class="btn btn-success btn-block" value="Buscar">
+              </div>
+          </form>
+        <?php }else if($filter == 'product_brand'){ ?>
+          <table class="table table-bordered" id="table">
+           <caption>Marca del Producto</caption>
+            <!-- CABECERA DE LA TABLA -->
+              <thead>
+                <tr class="text-center">
+                  <th>Marca</th>
+                  <th>Producto</th>
+                  <th>Contenido Neto (unidad)</th>
+                  <th>Cantidad Entrante (unidad)</th>
+                  <th>Precio de Compra (unidad)</th>
+                  <th>Porcentaje de Ganancia</th>
+                  <th>Precio Total (unidad)</th>
+                  <th>Acción</th>
+                </tr>
+              </thead>
+            <!-- CUERPO DE LA TABLA -->
+              <tbody> 
+                <?php 
+                  $query = "SELECT id, `product_name` AS PRODUCT, `product_brand` AS BRAND, product_size AS CONTENIDO_NETO, measure_unit AS PRESENTACION, units AS UNITS, buy_price AS BUY_PRICE, earn AS EARN, created_at AS DATE FROM inventario ORDER BY product_brand";
+                  $resultados = mysqli_query($conn, $query);
+                  $price = 0;
+                  $units = 0;
+                  $earn = 0;
+                  $IVA = 0;
+                  $total_price = 0;
+                  while($row = mysqli_fetch_assoc($resultados)) { 
+                    $price = (float)$row['BUY_PRICE']* (int)$row['UNITS'];
+                    $earn = $row['EARN']; 
+                    $total_price = $price + ($price * $earn) / 100 ;
+                    $IVA = $total_price * 0.16;
+                    $total_price = $total_price + $IVA; ?>
+                    <tr>
+                      <td> <?php echo $row['BRAND']; ?> </td>
+                      <td> <?php echo $row['PRODUCT']; ?> </td>
+                      <td> <?php echo $row['CONTENIDO_NETO'], $row['PRESENTACION']; ?> </td>
+                      <?php if( (int)$row['UNITS'] > 1){ ?>
+                          <td> <?php echo $row['UNITS'];?> unidades</td>
+                      <?php }else{ ?>
+                          <td> <?php echo $row['UNITS'];?> unidad</td> 
+                      <?php }; ?>
+                      <td> <?php echo $row['BUY_PRICE'];?>$</td>
+                      <td> <?php echo $row['EARN'];?>%</td>
+                      <td> <?php echo $total_price  / (int)$row['UNITS']?>$</td>
+                      <td>
+                        <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-primary">
+                          <i class="bi bi-pencil-fill"></i>
+                          ACTUALIZAR
+                        </a> 
+                        <br><br>
+                        <a href="delete.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                          <i class="bi bi-trash-fill"></i>
+                          ELIMINAR
+                        </a>
+                      </td>
+                    </tr>
+                <?php }; ?>        
+              </tbody>
+          </table>
+          <form action="index.php/#table" method="POST">
+            <!-- OPCIONES -->
+              <select name="filtrado">
+                <option value="product_name">Nombre del Producto</option>
+                <option value="product_brand" selected>Marca del Producto</option>
+                <option value="date">Fecha de Ingreso</option>
+              </select>
+            <!-- BOTON -->
+              <div class="form-group">
+                <input type="submit" name="filter" class="btn btn-success btn-block" value="Buscar">
+              </div>
+          </form>
+        <?php }else if($filter == 'date'){ ?>
+          <table class="table table-bordered" id="table">
+             <caption>Fecha de Añadido</caption>
+            <!-- CABECERA DE LA TABLA -->
+              <thead>
+                <tr class="text-center">
+                  <th>Añadido</th>
+                  <th>Producto</th>
+                  <th>Marca</th>
+                  <th>Contenido Neto (unidad)</th>
+                  <th>Cantidad Entrante (unidad)</th>
+                  <th>Precio de Compra (unidad)</th>
+                  <th>Porcentaje de Ganancia</th>
+                  <th>Precio Total (unidad)</th>
+                  <th>Acción</th>
+                </tr>
+              </thead>
+            <!-- CUERPO DE LA TABLA -->
+              <tbody> 
+                <?php 
+                  $query = "SELECT id, `product_name` AS PRODUCT, `product_brand` AS BRAND, product_size AS CONTENIDO_NETO, measure_unit AS PRESENTACION, units AS UNITS, buy_price AS BUY_PRICE, earn AS EARN, created_at AS ADDED FROM inventario ORDER BY created_at";
+                  $resultados = mysqli_query($conn, $query);
+                  $price = 0;
+                  $units = 0;
+                  $earn = 0;
+                  $IVA = 0;
+                  $total_price = 0;
+                  while($row = mysqli_fetch_assoc($resultados)) { 
+                    $price = (float)$row['BUY_PRICE']* (int)$row['UNITS'];
+                    $earn = $row['EARN']; 
+                    $total_price = $price + ($price * $earn) / 100 ;
+                    $IVA = $total_price * 0.16;
+                    $total_price = $total_price + $IVA; ?>
+                    <tr>
+                      <td> <?php echo $row['ADDED']; ?></td>
+                      <td> <?php echo $row['BRAND']; ?> </td>
+                      <td> <?php echo $row['PRODUCT']; ?> </td>
+                      <td> <?php echo $row['CONTENIDO_NETO'], $row['PRESENTACION']; ?> </td>
+                      <?php if( (int)$row['UNITS'] > 1){ ?>
+                          <td> <?php echo $row['UNITS'];?> unidades</td>
+                      <?php }else{ ?>
+                          <td> <?php echo $row['UNITS'];?> unidad</td> 
+                      <?php }; ?>
+                      <td> <?php echo $row['BUY_PRICE'];?>$</td>
+                      <td> <?php echo $row['EARN'];?>%</td>
+                      <td> <?php echo $total_price  / (int)$row['UNITS']?>$</td>
+                      <td>
+                        <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-primary">
+                          <i class="bi bi-pencil-fill"></i>
+                          ACTUALIZAR
+                        </a> 
+                        <br><br>
+                        <a href="delete.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                          <i class="bi bi-trash-fill"></i>
+                          ELIMINAR
+                        </a>
+                      </td>
+                    </tr>
+                <?php }; ?>        
+              </tbody>
+          </table>
+          <form action="index.php/#table" method="POST">
+            <!-- OPCIONES -->
+              <select name="filtrado">
+                <option value="product_name">Nombre del Producto</option>
+                <option value="product_brand">Marca del Producto</option>
+                <option value="date" selected>Fecha de Ingreso</option>
+              </select>
+            <!-- BOTON -->
+            <div class="form-group">
+              <input type="submit" name="filter" class="btn btn-success btn-block" value="Buscar">
+            </div>
+          </form>
+        <?php }else{ ?>
+          <strong>ERROR: INTERNO</strong>
+        <?php };?>
+        <?php };?>
     </div>
 </main>
 
